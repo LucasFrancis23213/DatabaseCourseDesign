f6e1b090675d4ce07849769078694788c5260ab1
LucasFrancis23213 <143398884+LucasFrancis23213@users.noreply.github.com>
2024-04-16 15:50:29 +0800

    clear ChenJiaHao branch

--- Templates/BasicSQLOPs/Utilities/DataCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿//用来存储所有创建的为了传递数据的数据结构
-namespace SQLOperation.Utilities
-{
-    public class UserInfo
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
