069922a4f7ae7ae21147d201d0aa9fb6e49f3f60
LucasFrancis23213 <143398884+LucasFrancis23213@users.noreply.github.com>
2024-05-13 20:35:35 +0800

    5.13 update

--- Templates/WebAppTest/obj/Debug/net8.0/WebAppTest.AssemblyInfo.cs
+++ Templates/WebAppTest/obj/Debug/net8.0/WebAppTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0ce484e47b5ff5a3e652111f27c66348c3cc9f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c177dbbf90e8ea8fb0664ea208271fc9df8919e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
