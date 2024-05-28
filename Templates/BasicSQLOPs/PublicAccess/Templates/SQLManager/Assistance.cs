5ff23d807aee35bcdb5dbc28b171c8855de9bb4d
LucasFrancis23213 <143398884+LucasFrancis23213@users.noreply.github.com>
2024-05-28 22:41:15 +0800

    5.28更新，重构后端代码结构

---
+++ Templates/BasicSQLOPs/PublicAccess/Templates/SQLManager/Assistance.cs
@@ -0,0 +1,54 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using SQLOperation.PublicAccess.Templates.TemplateInterfaceManager;
+using SqlSugar.Extensions;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Diagnostics;
+
+
+namespace SQLOperation.PublicAccess.Templates.SQLManager
+{
+    public class Assistance : IAssistance
+    {
+        private OracleConnection OracleConnection;
+        public Assistance(OracleConnection conn)
+        {
+            OracleConnection = conn;
+        }
+        public bool CheckColumnExists(string ColumnName, string TableName)
+        {
+            if (OracleConnection.State == ConnectionState.Open)
+            {
+                ColumnName = ColumnName.ToUpper();
+                TableName = TableName.ToUpper();
+                string Query = "SELECT COUNT(*) FROM ALL_TAB_COLUMNS WHERE TABLE_NAME = :TableName AND COLUMN_NAME = :ColumnName";
+                using (OracleCommand cmd = new OracleCommand(Query, OracleConnection))
+                {
+                    cmd.Parameters.Add(new OracleParameter(":TableName", TableName));
+                    cmd.Parameters.Add(new OracleParameter(":ColumnName", ColumnName));
+                    try
+                    {
+                        object result = cmd.ExecuteScalar();
+                        if (result != null)
+                        {
+                            int count = Convert.ToInt32(result);
+                            return count > 0;
+                        }
+                        return false;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        Debug.WriteLine(ex.ToString());
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                Debug.WriteLine("In CheckColumnExists Function，数据库未连接");
+                return false;
+            }
+        }
+    }
+}
---
+++ Templates/BasicSQLOPs/PublicAccess/Utilities/DataCollection.cs
@@ -0,0 +1,9 @@
+﻿//用来存储所有创建的为了传递数据的数据结构
+namespace SQLOperation.PublicAccess.Utilities
+{
+    public class UserInfo
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
