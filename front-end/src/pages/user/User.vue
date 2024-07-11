737da4476d142604da29037234991e715d294984
LucasFrancis23213 <143398884+LucasFrancis23213@users.noreply.github.com>
2024-07-11 18:19:23 +0800

    update gitignore file

--- front-end/src/pages/user/User.vue
+++
@@ -1,6 +0,0 @@
-<script lang="ts" setup>
-  import { ref } from 'vue';
-</script>
-<template>
-  <div class="user">用户管理</div>
-</template>
--- front-end/src/plugins/index.ts
+++
@@ -1,2 +0,0 @@
-export { useAuthStore, default as AuthPlugin } from './auth/auth-plugin';
-export { default as IconfontPlugin } from './iconfont';
