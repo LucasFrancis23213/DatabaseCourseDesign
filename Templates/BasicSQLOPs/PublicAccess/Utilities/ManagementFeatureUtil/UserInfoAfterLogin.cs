455c534a53a222e1bcd65b98420656307940c19f
PeiyuDuan <1395711570@qq.com>
2024-07-14 23:54:47 +0800

    更改用户登录接口

---
+++ Templates/BasicSQLOPs/PublicAccess/Utilities/ManagementFeatureUtil/UserInfoAfterLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using SQLOperation.PublicAccess.Utilities;
+
+namespace SQLOperation.PublicAccess.Utilities.ManagementFeatureUtil
+{
+    public class UserInfoAfterLogin
+    {
+
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Contact { get; set; }
+    }
+}
--- Templates/WebAppTest/APILayer/ManagementFeatureAPI/UserLoginController.cs
+++ Templates/WebAppTest/APILayer/ManagementFeatureAPI/UserLoginController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using SQLOperation.BusinessLogicLayer.ManagementFeatureBLL;
 using SQLOperation.PublicAccess.Utilities;
+using SQLOperation.PublicAccess.Utilities.ManagementFeatureUtil;
 
 namespace WebAppTest.APILayer.ManagementFeatureAPI
 {
@@ -15,12 +16,23 @@ namespace WebAppTest.APILayer.ManagementFeatureAPI
         }
 
         [HttpGet]
-        public IActionResult GetUserInfo(string UserName)
+        public IActionResult GetUserInfo(string UserName, string Password)
         {
             Tuple<bool, Users, string> result = UserLoginBLL.GetUserInfoUtil(UserName);
             if (result.Item1)
             {
-                return Ok(result.Item2);
+                if (result.Item2.Password == Password) 
+                {
+                    var retval = new UserInfoAfterLogin();
+                    retval.UserID = result.Item2.User_ID;
+                    retval.UserName = result.Item2.User_Name;
+                    retval.Contact = result.Item2.Contact;
+                    return Ok(retval); 
+                }
+                else
+                {
+                    return BadRequest(new { error = "用户名与密码不匹配"} );
+                }
             }
             else
             {
