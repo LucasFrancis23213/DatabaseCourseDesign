28470a9a2eae1b0f4e52418202ed7b16e6770ade
刘佳朋 <2395914432@qq.com>
2024-07-15 15:54:17 +0800

    表单v2

--- Resource/Files/lcbGroup/stepin-template/src/mock/account.js
+++ Resource/Files/lcbGroup/stepin-template/src/mock/account.js
@@ -1,5 +1,6 @@
 import Mock from 'mockjs';
 import CryptoJS from 'crypto-js';
+import dayjs from 'dayjs';
 
 function base64UrlEncode(source) {
   return CryptoJS.enc.Base64.stringify(source).replace(/=+$/, '').replace(/\+/g, '-').replace(/\//g, '_');
@@ -43,3 +44,59 @@ Mock.mock('api/account', 'get', ({}) => {
     },
   };
 });
+const arr = [
+  {
+    itemCategory: '手表',
+    itemDescribe: '劳力士',
+    lostPosition: '公园',
+    lostTime: '2024-07-01 15:30:48',
+    itemTags:['贵重物品', '私人用品', '医疗用品', '66666666'],
+    itemImage: '/src/assets/avatar/face-2.jpg',
+    isRewarded: true,
+    rewardAmount: '80000',
+    deadlineTime: '2024-07-01 15:30:48',
+    itemName: 'Li Zhi',
+  },
+  {
+    itemName: 'Li Zhi',
+    itemCategory: '手表',
+    itemDescribe: '劳力士',
+    lostPosition: '公园',
+    lostTime: '2024-07-01 15:30:48',
+    itemTags:['贵重物品'],
+    itemImage: '/src/assets/avatar/face-1.jpg',
+    isRewarded: false,
+    rewardAmount: '0',
+    deadlineTime: '无',
+  },
+]
+Mock.mock('api/publishs', 'get', () => arr)
+Mock.mock('api/addNewPublish', 'post', function(options){
+  const requestData = JSON.parse(options.body)
+  arr.push(requestData)
+})
+const arr2 = [
+  {
+    itemCategory: '手表',
+    itemDescribe: '劳力士',
+    findPosition: '公园',
+    findTime: '2024-07-01 15:30:48',
+    itemTags:['贵重物品', '私人用品', '医疗用品', '66666666'],
+    itemImage: '/src/assets/avatar/face-2.jpg',
+    itemName: 'Li Zhi',
+  },
+  {
+    itemName: 'Li Zhi',
+    itemCategory: '手表',
+    itemDescribe: '劳力士',
+    findPosition: '公园',
+    findTime: '2024-07-01 15:30:48',
+    itemTags:['贵重物品'],
+    itemImage: '/src/assets/avatar/face-1.jpg',
+  },
+]
+Mock.mock('api/finds', 'get', () => arr2)
+Mock.mock('api/addNewFind', 'post', function(options){
+  const requestData = JSON.parse(options.body)
+  arr2.push(requestData)
+})
--- Resource/Files/lcbGroup/stepin-template/src/pages/publishSearchNotice/PublishSearchNotice.vue
+++ Resource/Files/lcbGroup/stepin-template/src/pages/publishSearchNotice/PublishSearchNotice.vue
@@ -1,128 +1,86 @@
 <script lang="ts" setup>
   import { getBase64 } from '@/utils/file';
-  import { reactive, ref } from 'vue';
+  import { ref } from 'vue';
   import { FormInstance } from 'ant-design-vue';
-  import dayjs from 'dayjs';
-  import { Dayjs } from 'dayjs';
-import { message } from 'ant-design-vue';
-//表格的列
-  const columns = [
-    { title: '丢失物品', dataIndex: 'itemNameAndCategory'},
-    { title: '物品描述', dataIndex: 'itemDescribe', ellipsis: true},
-    { title: '遗失地点', dataIndex: 'lostPosition', ellipsis: true},
-    { title: '丢失日期', dataIndex: 'lostTime' },
-    { title: '物品标签', dataIndex: 'itemTags' },
-    { title: '物品图片', dataIndex: 'itemImage' },
-    { title: '是否悬赏', dataIndex: 'isRewarded' },
-    { title: '悬赏金额', dataIndex: 'rewardAmount' },
-    { title: '截止日期', dataIndex: 'deadlineTime' },
-  ];
+  import { message } from 'ant-design-vue';
+  import axios from 'axios';
+  import { onMounted } from 'vue';
+
   type onePublish = {
     itemName?: string;
     itemCategory?: string;
     itemDescribe?: string;
     lostPosition?: string;
-    lostTime?: Dayjs;
+    lostTime?: string;
     itemTags?:string[];
     itemImage?: string;
     isRewarded?: boolean;
     rewardAmount?: string;
-    deadlineTime?: Dayjs;
+    deadlineTime?: string;
   };
-
-  const publishs = reactive<onePublish[]>([
-    {
-      itemName: 'Li Zhi',
-      itemCategory: '手表',
-      itemDescribe: '劳力士',
-      lostPosition: '公园',
-      lostTime: dayjs(),
-      itemTags:['贵重物品', '私人用品', '医疗用品', '66666666'],
-      itemImage: '/src/assets/avatar/face-2.jpg',
-      isRewarded: true,
-      rewardAmount: '80000',
-      deadlineTime: dayjs(),
-    },
-    {
-      itemName: 'Li Zhi',
-      itemCategory: '手表',
-      itemDescribe: '劳力士',
-      lostPosition: '公园',
-      lostTime: dayjs(),
-      itemTags:['贵重物品'],
-      itemImage: '/src/assets/avatar/face-1.jpg',
-      isRewarded: false,
-      rewardAmount: '0',
-      deadlineTime: dayjs(),
-    },
-  ]);
-
+  const formModel = ref<FormInstance>();
+  const submit = () => {
+    loading.value = true;
+    formModel.value?.validateFields()
+      .then(async ()=>{
+        form.value.itemCategory = form.value.itemCategory[0]
+        await axios.post('api/addNewPublish', form.value)
+        getPublishs()
+        setTimeout(() => {
+          message.success('提交成功！');
+          loading.value = false;
+          open.value = false;
+        }, 2000);
+      })
+      .catch(()=>{
+        loading.value = false;
+        message.error('提交失败！')
+      })
+  };
+  const columns = [
+    { title: '丢失物品', dataIndex: 'itemNameAndCategory'},
+    { title: '物品描述', dataIndex: 'itemDescribe', ellipsis: true},
+    { title: '遗失地点', dataIndex: 'lostPosition', ellipsis: true},
+    { title: '丢失时间', dataIndex: 'lostTime' },
+    { title: '物品标签', dataIndex: 'itemTags' },
+    { title: '物品图片', dataIndex: 'itemImage' },
+    { title: '是否悬赏', dataIndex: 'isRewarded' },
+    { title: '悬赏金额', dataIndex: 'rewardAmount' },
+    { title: '截止时间', dataIndex: 'deadlineTime' },
+  ];
   const open = ref<boolean>(false);
   const loading = ref<boolean>(false);
   const addNew = () => {
     open.value = true;
   }
-  const submit = () => {
-    loading.value = true;
-    setTimeout(() => {
-      loading.value = false;
-    }, 2000);
-    formModel.value
-      ?.validateFields()
-      .then((res: onePublish) => {
-        publishs.push({ ...res });
-        message.success('提交成功！');
-        reset();
-        setTimeout(() => {
-          loading.value = false;
-          open.value = false;
-        }, 2000);
-      })
-      // .catch((e) => {
-      //   console.error(e);
-      // })
-  };
-  const newPublish = (publish?: onePublish) => {
-    publish.itemName = '';
-    publish.itemCategory = '';
-    publish.itemDescribe = '';
-    publish.lostPosition = '';
-    publish.lostTime = undefined;
-    publish.itemTags = [];
-    publish.itemImage = '';
-    publish.isRewarded = false;
-    publish.rewardAmount = '';
-    publish.deadlineTime = undefined;
-    return publish;
-  };
-  const reset = () => {
-    return newPublish(form);
-  }
-  const formModel = ref<FormInstance>();
-  const form = reactive<onePublish>({
+  const form = ref<onePublish>({
     itemName : '',
     itemCategory : '',
     itemDescribe : '',
     lostPosition : '',
-    lostTime : undefined,
+    lostTime : '',
     itemTags : [],
     itemImage : '',
     isRewarded : false,
-    rewardAmount : '',
-    deadlineTime : undefined,
+    rewardAmount : '1',
+    deadlineTime: '',
   });
-
   async function extractImg(file: Blob, publish: onePublish) {
     await getBase64(file).then((res) => {
       publish.itemImage = res;
     });
   }
-
   type IsRewarded = 0 | 1;
   const IsRewardedDict = {
     0: '未悬赏',
     1: '悬赏',
   };
+  const publishs = ref([])
+  const getPublishs = async () => {
+    const res = await axios.get('api/publishs')
+    publishs.value = res.data
+  }
+  onMounted(() => getPublishs())
 </script>
 
 <template>
@@ -130,19 +88,19 @@ import { message } from 'ant-design-vue';
     <template #footer></template>
     <a-form ref="formModel" :model="form" :labelCol="{ span: 5 }" :wrapperCol="{ span: 16 }" >
       <a-form-item label="物品名称" name="itemName" has-feedback :rules="[{ required: true, message: '请输入物品名称' }]">
-        <a-input v-model:value="form.itemName" maxlength="20" />
+        <a-input v-model:value="form.itemName" :maxlength="20" />
       </a-form-item>
       <a-form-item label="物品类别" name="itemCategory" has-feedback :rules="[{ required: true, message: '请选择物品类别' }]">
         <a-cascader v-model:value="form.itemCategory" :options="[{label: '物品类别1', value: '物品类别1',}, {label: '手表', value: '手表',},]"/>
       </a-form-item>
       <a-form-item label="物品描述" name="itemDescribe" has-feedback :rules="[{ required: true, message: '请输入物品描述' }]">
-        <a-textarea :rows="4" v-model:value="form.itemDescribe" maxlength="100" />
+        <a-textarea :rows="4" v-model:value="form.itemDescribe" :maxlength="100" />
       </a-form-item>
       <a-form-item label="丢失地点" name="lostPosition" has-feedback :rules="[{ required: true, message: '请输入丢失地点' }]">
-        <a-textarea :rows="4" v-model:value="form.lostPosition" maxlength="100" />
+        <a-textarea :rows="4" v-model:value="form.lostPosition" :maxlength="100" />
       </a-form-item>
-      <a-form-item label="丢失日期" name="lostTime" has-feedback :rules="[{ required: true, message: '请输入丢失日期' }]">
-        <a-date-picker v-model:value="form.lostTime" />
+      <a-form-item label="丢失时间" name="lostTime" has-feedback :rules="[{ required: true, message: '请输入丢失时间' }]">
+        <a-date-picker v-model:value="form.lostTime" show-time/>
       </a-form-item>
       <a-form-item label="物品标签" name="itemTags" has-feedback :rules="[{ required: true, message: '请选择物品标签' }]">
       <a-checkbox-group v-model:value="form.itemTags">
@@ -166,11 +124,11 @@ import { message } from 'ant-design-vue';
         <a-switch v-model:checked="form.isRewarded" />
       </a-form-item>
       <template v-if="form.isRewarded">
-        <a-form-item label="悬赏金额" name="rewardAmount" has-feedback :rules="[{ required: true, message: '请输入悬赏金额' }]">
-          <a-input v-model:value="form.rewardAmount" maxlength="10"/>
+        <a-form-item label="悬赏金额" name="rewardAmount" has-feedback>
+          <a-input-number v-model:value="form.rewardAmount" prefix="￥" :min="1" :max="9999999999" />
         </a-form-item>
-        <a-form-item label="悬赏截止日期" name="deadlineTime" has-feedback :rules="[{ required: true, message: '请输入悬赏截止日期' }]">
-          <a-date-picker v-model:value="form.deadlineTime" />
+        <a-form-item label="悬赏截止时间" name="deadlineTime" has-feedback :rules="[{ required: true, message: '请输入悬赏截止时间' }]">
+          <a-date-picker v-model:value="form.deadlineTime" show-time/>
         </a-form-item>
       </template>
       <a-form-item :wrapper-col="{ offset: 10, span: 16 }">
@@ -193,23 +151,17 @@ import { message } from 'ant-design-vue';
       </div>
     </template>
     <template #bodyCell="{ column, record }">
-      <div class="" v-if="column.dataIndex === 'itemNameAndCategory'">
+      <template v-if="column.dataIndex === 'itemNameAndCategory'">
         <div class="text-title font-bold">
           {{ record.itemName }}
         </div>
         <div class="text-subtext">
-          {{ record.itemCategory }}
+          {{record.itemCategory}}
         </div>
-      </div>
-      <template v-else-if="column.dataIndex === 'lostTime'">
-        {{ record.lostTime?.format('YYYY-MM-DD') }}
       </template>
-      <template v-else-if="column.dataIndex === 'deadlineTime'">
-        {{ record.deadlineTime?.format('YYYY-MM-DD') }}
-      </template>
-      <div class="" v-else-if="column.dataIndex === 'itemImage'">
+      <template v-else-if="column.dataIndex === 'itemImage'">
         <img class="w-12 rounded" :src="record.itemImage" />
-      </div>
+      </template>
       <template v-else-if="column.dataIndex === 'isRewarded'">
         <a-badge class="text-subtext" :color="'green'">
           <template #text>
