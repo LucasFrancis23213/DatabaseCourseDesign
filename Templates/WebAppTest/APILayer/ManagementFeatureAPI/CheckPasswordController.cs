363a2d6d24dd618548de93ca4b752e81cc0369ec
PeiyuDuan <1395711570@qq.com>
2024-07-16 15:14:42 +0800

    添加CheckPassword API的返回状况

--- Templates/WebAppTest/APILayer/ManagementFeatureAPI/CheckPasswordController.cs
+++ Templates/WebAppTest/APILayer/ManagementFeatureAPI/CheckPasswordController.cs
@@ -23,7 +23,15 @@ namespace WebAppTest.APILayer.ManagementFeatureAPI
             }
             else
             {
-                return BadRequest(result.Item2);
+                switch (result.Item2)
+                {
+                    case "用户名与密码不匹配":
+                        return Unauthorized(result.Item2);
+                    case "Users表没有符合要求的元素":
+                        return NotFound(result.Item2);
+                    default:
+                        return BadRequest(result.Item2);
+                }
             }
         }
     }
