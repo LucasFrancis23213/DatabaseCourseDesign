f41733d2dbf9ca7c63d2c9497c8052e75a4aa7ef
bambooshoot2 <145686622+bambooshoot2@users.noreply.github.com>
2024-07-16 23:30:41 +0800

    增加使用的数据结构

---
+++ 数据库第一周后端代码 2024.7.16/CommunityFeatureDataCollection.cs
@@ -0,0 +1,10 @@
+﻿namespace SQLOperation.PublicAccess.Utilities
+{
+    public class Conversation_User_Message {
+        public User_Conversations userConversations {  get; set; }
+        public User_Messages userLastMessage {  get; set; }
+        public Users userInfo {  get; set; }
+    }
+
+
+}
