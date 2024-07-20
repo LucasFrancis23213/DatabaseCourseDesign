8890beb763d32dfed5e987cabecba38580ff90ef
liujiapeng678 <jj0723938@gmail.com>
2024-07-20 17:10:48 +0800

    更新分支内容

---
+++ Templates/WebAppTest/Program.cs
@@ -0,0 +1,38 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAllOrigins"); // Use the CORS policy
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
