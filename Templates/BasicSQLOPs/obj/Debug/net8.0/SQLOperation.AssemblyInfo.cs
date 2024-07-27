faefedbf95ae9dae12fad2ded3e803086d8320c7
charlotte <757847466@qq.com>
2024-07-27 11:55:49 +0800

    增加gitignore文件

--- Templates/BasicSQLOPs/obj/Debug/net8.0/SQLOperation.AssemblyInfo.cs
+++ Templates/BasicSQLOPs/obj/Debug/net8.0/SQLOperation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQLOperation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd334151a1f274382653b7d74aae5c7d1729f2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+914f7180ccce941d8d94427952f1077747902e43")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQLOperation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQLOperation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
