e57f231fae9f0d2b7a0d0b4deeb172a5098f8eb2
PeiyuDuan <1395711570@qq.com>
2024-08-02 00:12:34 +0800

    清理解决方案，便于合并

--- Templates/BasicSQLOPs/obj/Debug/net8.0/SQLOperation.AssemblyInfo.cs
+++ Templates/BasicSQLOPs/obj/Debug/net8.0/SQLOperation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQLOperation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5ccbc2f27708e073cb3f2ad09b81dda0e1f7ab5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab65ac1816be942d2e1b83ba2bfe87e86075307")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQLOperation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQLOperation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
