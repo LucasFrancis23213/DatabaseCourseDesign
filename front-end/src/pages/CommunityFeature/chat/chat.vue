0b081324a4427e91d50b31b76ed3f66659b4eabe
Eurekahah <A984036428@gmail.com>
2024-08-23 16:38:36 +0800

    1. 修改时间显示的bug

--- front-end/src/pages/CommunityFeature/chat/chat.vue
+++ front-end/src/pages/CommunityFeature/chat/chat.vue
@@ -1,19 +1,20 @@
 <script lang="ts" setup>
-import {onMounted, onUnmounted, onUpdated, ref,nextTick} from 'vue';
+import {onMounted, onUnmounted, onUpdated, ref, nextTick} from 'vue';
 import ChatBubble from "@/components/CommunityFeature/chat/ChatBubble.vue";
 import MessageInput from "@/components/CommunityFeature/chat/MessageInput.vue";
 import apiService from './apiService';
 import axios from "axios";
 import {useRoute} from 'vue-router';
-import { useAccountStore } from '@/store/account';
+import {useAccountStore} from '@/store/account';
+
 const {account, permissions} = useAccountStore();
 
 
-axios.defaults.baseURL= import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
-const route =useRoute();
+const route = useRoute();
 
-const conversation_id  = ref(+route.params.conversation_id);
+const conversation_id = ref(+route.params.conversation_id);
 const current_user_id = ref(+account.userId);
 
 const other_avatar = ref();
@@ -33,22 +34,24 @@ const getMessages = async () => {
     alert("获取消息列表出现错误");
   }
 };
+const beijingTime = new Date(new Date().getTime() + 8 * 3600 * 1000);
+const formattedTime = beijingTime.toISOString().replace('Z', '+08:00');
+
 async function handleSendMessage(message) {
   if (!message.trim()) {
     return; // 如果消息为空，则不发送
   }
   // 创建消息对象
   const newMessage = {
-    id: parseInt(Date.now().toString()) , // 使用当前时间戳作为临时ID
+    id: parseInt(Date.now().toString()), // 使用当前时间戳作为临时ID
     conversation_id: conversation_id.value,
     content: message,
-    type:'text',
-    time: new Date().toISOString(),
-    sender:+current_user_id.value,
+    type: 'text',
+    time: formattedTime,
+    sender: +current_user_id.value,
     current_user_id: +current_user_id.value, // 假设这是用户发送的消息
   };
-  // console.log(newMessage);
-  // 在前端立即显示消息（回显）
+  console.log(newMessage.time)
   displayMessage(newMessage);
 
   try {
@@ -57,12 +60,13 @@ async function handleSendMessage(message) {
 
     // 如果后台返回了更新后的消息（例如，带有服务器生成的ID），则更新前端显示
     if (response) {
-      updateDisplayedMessage(newMessage.id,response.message_id);
+      updateDisplayedMessage(newMessage.id, response.message_id);
     }
   } catch (error) {
     alert("消息发送失败，请稍后重试");
   }
 }
+
 function displayMessage(message) {
   // 实现显示消息的逻辑
   messages.value.push(message);
@@ -70,34 +74,35 @@ function displayMessage(message) {
 
 
 // 辅助函数：更新已显示的消息的id
-function updateDisplayedMessage(oldId,newId) {
+function updateDisplayedMessage(oldId, newId) {
   const oldMessage = messages.value.filter(msg => msg.id === oldId).shift();
   oldMessage.id = newId;
 
 }
 
-function isSelf(sender_id){
+function isSelf(sender_id) {
   return sender_id === current_user_id.value;
 }
-async function updateReadStatus(){
+
+async function updateReadStatus() {
   /*更新消息的读取状态*/
-  try{
-    await apiService.updateReadStatus(conversation_id.value,current_user_id.value);
-  }catch(e){
+  try {
+    await apiService.updateReadStatus(conversation_id.value, current_user_id.value);
+  } catch (e) {
     alert(`更新已读状态失败，错误信息为：${e}`);
   }
 }
 
-async function retractMessage(message_id){
+async function retractMessage(message_id) {
   /*撤回消息*/
   // 向后台发送消息处理撤回逻辑
-  try{
-    await apiService.retractMessage(message_id,current_user_id.value);
-  }catch (e){
+  try {
+    await apiService.retractMessage(message_id, current_user_id.value);
+  } catch (e) {
     alert(`撤回失败，请重试`);
   }
   // 前台移除该消息
-  messages.value = messages.value.filter(msg => msg.id !== message_id)
+  removeMessage(message_id)
 }
 
 const connection = ref(null);
@@ -115,7 +120,7 @@ const connect = () => {
 
   socket.value.onclose = () => {
     connectionStatus.value = 'Disconnected';
-    setTimeout(connect,3000);//断连后三秒重连
+    setTimeout(connect, 3000);//断连后三秒重连
   };
 
   socket.value.onerror = (error) => {
@@ -126,6 +131,7 @@ const connect = () => {
     receiveMessage(event.data);
   };
 };
+
 function convertMessageFormat(originalMessage) {
   //解析接收消息的格式并更改为此组件中使用的message格式
   const msg = JSON.parse(originalMessage);
@@ -140,10 +146,19 @@ function convertMessageFormat(originalMessage) {
     current_user_id: msg.Receiver_User_ID
   };
 }
+
+function removeMessage(message_id: Number) {
+  messages.value = messages.value.filter(msg => msg.id !== message_id)
+}
+
 const receiveMessage = (message) => {
   //接收消息
   let newMessage = convertMessageFormat(message);
-  displayMessage(newMessage);
+  if (newMessage.type === 'retract') {
+    removeMessage(newMessage.id);
+  } else if (newMessage.type === 'text') {
+    displayMessage(newMessage);
+  }
 };
 const disconnect = () => {
   //断开连接
@@ -184,24 +199,21 @@ const scrollToBottom = () => {
   })
 }
 
-onMounted(()=>{
+onMounted(() => {
   updateReadStatus();
   getMessages();
   scrollToBottom();
   connect();
 })
 
-onUpdated(()=>{
+onUpdated(() => {
   scrollToBottom();
 
 })
 
-onUnmounted(()=>{
+onUnmounted(() => {
   disconnect();
 })
-
-
-
 
 
 </script>
@@ -210,24 +222,24 @@ onUnmounted(()=>{
   <div class="chat-container" ref="chatContainerRef">
     <div class="messages-list">
       <ChatBubble
-        v-for="msg in messages"
-        :key="msg.id"
-        :id="msg.id"
-        :content="msg.content"
-        :time="msg.time"
-        :isSelf="isSelf(msg.sender)"
-        :avatar="isSelf(msg.sender) ? '' :other_avatar"
-        :type="msg.type"
-        @follow=""
-        @retract="retractMessage"
+          v-for="msg in messages"
+          :key="msg.id"
+          :id="msg.id"
+          :content="msg.content"
+          :time="msg.time"
+          :isSelf="isSelf(msg.sender)"
+          :avatar="isSelf(msg.sender) ? '' :other_avatar"
+          :type="msg.type"
+          @follow=""
+          @retract="retractMessage"
       />
     </div>
     <button @click="checkConnection">检查连接</button>
 
     <MessageInput
-      :is-sending="isSending"
-      @send-message="handleSendMessage"
-      class="message-input"
+        :is-sending="isSending"
+        @send-message="handleSendMessage"
+        class="message-input"
     />
   </div>
 </template>
@@ -247,7 +259,7 @@ onUnmounted(()=>{
   margin-bottom: 20px;
 }
 
-.message-input{
+.message-input {
 
   position: absolute;
   bottom: 0;
