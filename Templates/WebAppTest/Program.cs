0f1449e78cfeea3d8a2618e787f889d3497d6542
LucasFrancis23213 <143398884+LucasFrancis23213@users.noreply.github.com>
2024-08-28 11:32:23 +0800

    commit current change in dec

--- Templates/WebAppTest/Program.cs
+++ Templates/WebAppTest/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // 配置 Kestrel 只使用 HTTP
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
-serverOptions.Listen(IPAddress.Any, 5174);  // 使用端口 5000，您可以根据需要更改
+    serverOptions.Listen(IPAddress.Any, 5174);  // 使用端口 5000，您可以根据需要更改
 });
 
 // Learn more about configuring Swagger/OpenAPI at
@@ -42,7 +42,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
