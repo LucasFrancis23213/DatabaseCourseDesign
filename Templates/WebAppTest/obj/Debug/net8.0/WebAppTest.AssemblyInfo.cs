b9c6c69ba66e8483d25adaa8c1e1295a34daa342
LucasFrancis23213 <143398884+LucasFrancis23213@users.noreply.github.com>
2024-08-28 11:44:31 +0800

    merge lcb-dev from remote repo

--- Templates/WebAppTest/obj/Debug/net8.0/WebAppTest.AssemblyInfo.cs
+++ Templates/WebAppTest/obj/Debug/net8.0/WebAppTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c9f3685c96c22533ff29e0ac2a57fa12c5d898")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a26349d0fd7369984566c0d6573f419a380605")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
