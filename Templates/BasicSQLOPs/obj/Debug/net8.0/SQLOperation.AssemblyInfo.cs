54ef0f43e8541443ab4e83534e580a06e982ead8
LucasFrancis23213 <143398884+LucasFrancis23213@users.noreply.github.com>
2024-09-05 19:02:05 +0800

    add

--- Templates/BasicSQLOPs/obj/Debug/net8.0/SQLOperation.AssemblyInfo.cs
+++ Templates/BasicSQLOPs/obj/Debug/net8.0/SQLOperation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQLOperation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2dc7c4ab5ab4c8f97e9e6dfd3f6d3d082e1ebe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19482e918f62704dbb2579c7b59dacb9f1db4be5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQLOperation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQLOperation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Templates/WebAppTest/obj/Debug/net8.0/WebAppTest.AssemblyInfo.cs
+++ Templates/WebAppTest/obj/Debug/net8.0/WebAppTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2dc7c4ab5ab4c8f97e9e6dfd3f6d3d082e1ebe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19482e918f62704dbb2579c7b59dacb9f1db4be5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
